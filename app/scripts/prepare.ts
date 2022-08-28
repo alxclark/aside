1dbe72c589f46b09a9bb4e48eaeaf732ff17a101
Alex Clark <alex.clark.015@gmail.com>
2022-08-28 19:26:28 -0400

    Fix missing React imports and add Recoil example

--- app/scripts/prepare.ts
+++ app/scripts/prepare.ts
@@ -19,6 +19,7 @@ async function stubIndexHtml() {
     let data = await fs.readFile(r(`src/${view}/index.html`), 'utf-8')
     data = data
       .replace('"./main.tsx"', `"http://localhost:${port}/${view}/main.tsx"`)
+      .replace('"./main.ts"', `"http://localhost:${port}/${view}/main.ts"`)
       .replace('<div id="app"></div>', '<div id="app">Vite server did not start</div>')
     await fs.writeFile(r(`extension/dist/${view}/index.html`), data, 'utf-8')
     log('PRE', `stub ${view}`)
--- app/src/contentScripts/index.tsx
+++ app/src/contentScripts/index.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable no-console */
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { onMessage } from 'webext-bridge'
 import { App } from './views/App'
