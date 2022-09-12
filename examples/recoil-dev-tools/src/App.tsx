9223dee7a669229698499e78d2c7895d7e6d386a
Alex Clark <alex.clark.015@gmail.com>
2022-09-11 22:19:24 -0400

    Progress on communication

--- examples/recoil-dev-tools/src/App.tsx
+++ examples/recoil-dev-tools/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import {Companion} from '@companion/react';
 
 export function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +18,7 @@ export function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Companion />
     </div>
   )
 }
--- examples/recoil-dev-tools/vite.config.ts
+++ examples/recoil-dev-tools/vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), AutoImport({
+    imports: [
+      {
+        'webextension-polyfill': [
+          ['*', 'browser'],
+        ],
+      },
+    ],
+  }),],
   server: {
     port: 3000
   }
