e7bc024b9702ed3708b94833e4a5597a7a111ced
Alex Clark <alex.clark.015@gmail.com>
2023-02-18 20:00:03 -0500

    Fix default style for chrome-ui stories and add Log story

---
+++ packages/chrome-ui/src/components/Log/Log.stories.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react';
+
+import {Log, Props} from './Log';
+
+import '../../../dist/styles.css';
+
+export default {
+  title: 'ChromeUI/Log',
+  component: Log,
+};
+
+const Template = (args: any) => <Log {...args} />;
+
+export const Default = Template.bind({}) as any;
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  items: [{id: 'first', value: {something: {1: 'hey'}, else: 5}}],
+} as Props;
