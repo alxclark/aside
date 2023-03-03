1b4c0ec0d772ca271f66d67f8cb75854fd71f323
Alexandre Clark <alex.clark.015@gmail.com>
2023-03-02 21:27:21 -0500

    WIP turborepo

--- app/src/pages/popup/index.tsx
+++ app/src/pages/popup/index.tsx
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import {Popup} from './Popup';
-import '../styles';
 
 ReactDOM.render(<Popup />, document.getElementById('app') as HTMLElement);
