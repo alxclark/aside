9394d06ef47c93a0583c5300849a51e03a064ae9
Alexandre Clark <alex.clark.015@gmail.com>
2023-03-04 14:58:28 -0500

    Chrome-ui setup

--- packages/chrome-ui/vite.config.remote.ts
+++ packages/chrome-ui/vite.config.remote.ts
@@ -5,11 +5,12 @@ import {defineConfig} from 'vite';
 export default defineConfig({
   build: {
     emptyOutDir: false,
+    outDir: 'build/remote/js',
     lib: {
       entry: resolve(__dirname, 'src/remote/index.ts'),
       name: 'chrome-ui',
       formats: ['es', 'umd'],
-      fileName: (format) => `chrome-ui-remote.${format}.js`,
+      fileName: (format) => `chrome-ui.${format}.js`,
     },
     rollupOptions: {
       external: ['react'],
--- packages/chrome-ui/vite.config.ts
+++ packages/chrome-ui/vite.config.ts
@@ -5,11 +5,12 @@ import {defineConfig} from 'vite';
 export default defineConfig({
   build: {
     emptyOutDir: false,
+    outDir: 'build/react/js',
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'chrome-ui',
       formats: ['es', 'umd'],
-      fileName: (format) => `chrome-ui-react.${format}.js`,
+      fileName: (format) => `chrome-ui.${format}.js`,
     },
     rollupOptions: {
       external: ['react'],
