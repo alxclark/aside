274938afbfdbe11e33623b18408ecc4c96163080
Alexandre Clark <alex.clark.015@gmail.com>
2023-04-13 20:29:59 -0400

    Refactor recoil host and remote to track snapshots when devtools is closed and to pass it to the app when mounting

--- packages/recoil/src/host.tsx
+++ packages/recoil/src/host.tsx
@@ -1,20 +1,128 @@
-import React, {PropsWithChildren} from 'react';
+import React, {
+  PropsWithChildren,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
 import {Aside, DevTools as AsideDevTools} from '@aside/react';
-import {RecoilRoot, useRecoilSnapshot} from 'recoil';
+import {
+  RecoilRoot,
+  useRecoilSnapshot,
+  Snapshot as RecoilSnapshot,
+} from 'recoil';
 
 import {RemoteDevTools} from './remote';
+import {
+  currentStateAtom,
+  diffsAtom,
+  initialStateAtom,
+  Snapshot,
+  snapshotsAtom,
+} from './foundation/Snapshots';
+import {isInternalAtom} from './utilities/recoil';
 
 export function DevTools({children}: PropsWithChildren<{}>) {
   const recoilSnapshot = useRecoilSnapshot();
+  const {snapshot, diff} = useMemo(
+    () => transformSnapshot(recoilSnapshot),
+    [recoilSnapshot],
+  );
+
+  const [shouldRecordSnapshots, setShouldRecordSnapshots] = useState(true);
+  const [diffs, setDiffs] = useState<Snapshot[]>([]);
+  const [snapshots, setSnapshots] = useState<Snapshot[]>([]);
+
+  // Persist the state snapshots client side.
+  // This is needed since we want access to the previous states
+  // even if the devtools panel was not opened.
+  useEffect(() => {
+    // Once the devtools is opened and the remote is mounted,
+    // we do not need to record snapshots on the client and on the remote.
+    // Only communicating a single snapshot at a time over the bridge
+    // is less expensive then passing a complete array of all snapshots everytime
+    // there is a change.
+    if (shouldRecordSnapshots) {
+      setSnapshots((prev) => [...prev, snapshot]);
+      setDiffs((prev) => [...prev, diff]);
+    }
+  }, [setDiffs, snapshot, diff, shouldRecordSnapshots]);
+
+  const handleMount = useCallback(() => {
+    setShouldRecordSnapshots(false);
+  }, []);
+
+  const handleUnMount = useCallback(() => {
+    setShouldRecordSnapshots(true);
+  }, []);
 
   return (
     <Aside>
       <AsideDevTools>
-        <RecoilRoot key="@aside/recoil">
-          <RemoteDevTools snapshot={recoilSnapshot} />
+        <RecoilRoot
+          key="@aside/recoil"
+          initializeState={(snapshot) => {
+            snapshot.set(snapshotsAtom, snapshots);
+            snapshot.set(initialStateAtom, snapshots[0]);
+            snapshot.set(currentStateAtom, snapshots[snapshots.length - 1]);
+
+            const [_emptyDiff, ...rest] = diffs;
+            snapshot.set(diffsAtom, [snapshots[0], ...rest]);
+          }}
+        >
+          <RemoteDevTools
+            snapshot={snapshot}
+            diff={diff}
+            onMount={handleMount}
+            onUnmount={handleUnMount}
+          />
         </RecoilRoot>
         {children}
       </AsideDevTools>
     </Aside>
   );
+}
+
+// Recoil snapshots need to be manually retained in order to extract the data from it.
+// In order to make it simpler to pass it to the remote, we pre-process
+// each snapshot and generate an object containing all values and a diff of the state.
+function transformSnapshot(recoilSnapshot: RecoilSnapshot) {
+  const createdAt = new Date();
+
+  const snapshot: Snapshot = {
+    id: recoilSnapshot.getID(),
+    createdAt,
+    nodes: {},
+  };
+
+  for (const node of recoilSnapshot.getNodes_UNSTABLE()) {
+    if (isInternalAtom(node.key)) continue;
+
+    const parts = node.key.split('__');
+    const hasAtomFamilyPrefix = parts.length > 1;
+    if (hasAtomFamilyPrefix) {
+      const [prefix, ...rest] = parts;
+      const itemKey = rest.join('').replaceAll('"', '');
+
+      snapshot.nodes[prefix] ||= {};
+      snapshot.nodes[prefix][itemKey] = recoilSnapshot
+        .getLoadable(node)
+        .getValue();
+    } else {
+      snapshot.nodes[node.key] = recoilSnapshot.getLoadable(node).getValue();
+    }
+  }
+
+  const diff: Snapshot = {
+    id: recoilSnapshot.getID(),
+    createdAt,
+    nodes: {},
+  };
+
+  for (const node of recoilSnapshot.getNodes_UNSTABLE({isModified: true})) {
+    if (isInternalAtom(node.key)) continue;
+    diff.nodes[node.key] = recoilSnapshot.getLoadable(node).getValue();
+  }
+
+  return {diff, snapshot};
 }
--- packages/recoil/src/remote.tsx
+++ packages/recoil/src/remote.tsx
@@ -1,91 +1,64 @@
 import React, {useEffect} from 'react';
-import {
-  Snapshot as RecoilSnapshot,
-  useRecoilState,
-  useRecoilValue,
-  useSetRecoilState,
-} from 'recoil';
+import {useRecoilValue, useSetRecoilState} from 'recoil';
 
 import {PrimaryPanel} from './components';
-import {isInternalAtom} from './utilities/recoil';
 import {
   currentStateAtom,
   diffsAtom,
-  initialStateAtom,
   recordSnapshotAtom,
   Snapshot,
   snapshotsAtom,
 } from './foundation/Snapshots';
 
 export function RemoteDevTools({
-  snapshot: recoilSnapshot,
+  onMount,
+  onUnmount,
+  snapshot,
+  diff,
 }: {
-  snapshot: RecoilSnapshot;
+  onMount(): void;
+  onUnmount(): void;
+  snapshot: Snapshot;
+  diff: Snapshot;
 }) {
   const shouldRecordSnapshot = useRecoilValue(recordSnapshotAtom);
   const setSnapshots = useSetRecoilState(snapshotsAtom);
   const setDiffs = useSetRecoilState(diffsAtom);
   const setCurrentState = useSetRecoilState(currentStateAtom);
-  const [initialState, setInitialState] = useRecoilState(initialStateAtom);
 
   useEffect(() => {
-    const createdAt = new Date();
+    onMount();
 
-    const snapshot: Snapshot = {
-      id: recoilSnapshot.getID(),
-      createdAt,
-      nodes: {},
+    return () => {
+      onUnmount();
     };
+  }, [onMount, onUnmount]);
 
-    if (initialState?.id === snapshot.id) return;
-
-    for (const node of recoilSnapshot.getNodes_UNSTABLE()) {
-      if (isInternalAtom(node.key)) continue;
-
-      const parts = node.key.split('__');
-      const hasAtomFamilyPrefix = parts.length > 1;
-      if (hasAtomFamilyPrefix) {
-        const [prefix, ...rest] = parts;
-        const itemKey = rest.join('').replaceAll('"', '');
-
-        snapshot.nodes[prefix] ||= {};
-        snapshot.nodes[prefix][itemKey] = recoilSnapshot
-          .getLoadable(node)
-          .getValue();
-      } else {
-        snapshot.nodes[node.key] = recoilSnapshot.getLoadable(node).getValue();
-      }
-    }
-
-    const diff: Snapshot = {
-      id: recoilSnapshot.getID(),
-      createdAt,
-      nodes: {},
-    };
-
-    for (const node of recoilSnapshot.getNodes_UNSTABLE({isModified: true})) {
-      if (isInternalAtom(node.key)) continue;
-      diff.nodes[node.key] = recoilSnapshot.getLoadable(node).getValue();
-    }
-
+  useEffect(() => {
     setCurrentState(snapshot);
-
-    if (!initialState) {
-      setInitialState(snapshot);
-    }
 
     if (!shouldRecordSnapshot) return;
 
-    setSnapshots((prev) => [...prev, snapshot]);
-    setDiffs((prev) => [...prev, initialState ? diff : snapshot]);
+    setSnapshots((prev) => {
+      if (prev[prev.length - 1]?.id === snapshot.id) {
+        return prev;
+      }
+      return [...prev, snapshot];
+    });
+
+    setDiffs((prev) => {
+      if (prev[prev.length - 1]?.id === diff.id) {
+        return prev;
+      }
+      return [...prev, diff];
+    });
   }, [
-    initialState,
-    recoilSnapshot,
+    diff,
     setCurrentState,
     setDiffs,
-    setInitialState,
     setSnapshots,
     shouldRecordSnapshot,
+    snapshot,
   ]);
 
   return <PrimaryPanel />;
