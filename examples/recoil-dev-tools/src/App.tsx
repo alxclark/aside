d3803a9fe66fa50843585c8a73e085e261c5e1d7
Alexandre Clark <alex.clark.015@gmail.com>
2023-04-20 20:13:58 -0400

    Update example to have a randomize to go cycle between different types

--- examples/recoil-dev-tools/src/App.tsx
+++ examples/recoil-dev-tools/src/App.tsx
@@ -25,10 +25,35 @@ export function RecoilApp() {
   const object = useRecoilValue(objectAtom);
   const family = useRecoilValue(familyAtom('1'));
   const family2 = useRecoilValue(familyAtom('2'));
+  const [random, setRandom] = useRecoilState(randomAtom);
+
+  function randomize() {
+    switch (typeof random) {
+      case 'number':
+        setRandom('ninety-nine');
+        break;
+      case 'string':
+        setRandom({ninety: {nine: 9}});
+        break;
+      case 'object':
+        setRandom(() => () => 99);
+        break;
+      case 'function':
+        setRandom(true);
+        break;
+      case 'boolean':
+        setRandom(undefined);
+        break;
+      case 'undefined':
+        setRandom(null);
+        break;
+    }
+  }
 
   return (
     <div className="App">
       <h1>Vite + React</h1>
+      <button onClick={() => randomize()}>Randomize</button>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -52,6 +77,11 @@ export function RecoilApp() {
 const countAtom = atom({
   default: 0,
   key: 'count',
+});
+
+const randomAtom = atom<any>({
+  key: 'random',
+  default: 99,
 });
 
 const countTimesTenAtom = selector({
