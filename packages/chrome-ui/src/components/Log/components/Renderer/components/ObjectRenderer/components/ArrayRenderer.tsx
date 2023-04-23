0f82461d860aa4f360b8c0f5655eac37bd4f001c
Alexandre Clark <alex.clark.015@gmail.com>
2023-04-23 14:44:30 -0400

    Complete array renderer

--- packages/chrome-ui/src/components/Log/components/Renderer/components/ObjectRenderer/components/ArrayRenderer.tsx
+++ packages/chrome-ui/src/components/Log/components/Renderer/components/ObjectRenderer/components/ArrayRenderer.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
+import classNames from 'classnames';
 
 // eslint-disable-next-line import/no-cycle
 import {Renderer} from '../../../Renderer';
-// eslint-disable-next-line import/no-cycle
-import {ObjectRenderer} from '../ObjectRenderer';
+import {Carret} from '../../../../../../Carret';
 
 export function ArrayRenderer({
   value,
@@ -16,18 +16,88 @@ export function ArrayRenderer({
   preview?: boolean;
   path: string[];
 }) {
+  const [collapsed, setCollapsed] = useState(true);
+
   if (preview) {
-    return <span className="text-white">Array({value.length})</span>;
+    return <SimplePreview length={value.length} />;
   }
 
   const key = path[path.length - 1];
 
   return (
-    <ObjectRenderer
-      path={[]}
-      value={{[key]: {...value}}}
-      preview={preview}
-      showDiffs={showDiffs}
-    />
+    <>
+      <button
+        className="ml-[-10px] text-left"
+        onClick={() => setCollapsed((prev) => !prev)}
+      >
+        <Carret direction={collapsed ? 'right' : 'down'} />
+        {path.length > 0 && (
+          <>
+            <span className="text-console-object-blue font-bold">{key}</span>
+            {': '}
+            {collapsed ? (
+              <DescriptivePreview value={value} showDiffs={showDiffs} />
+            ) : (
+              <SimplePreview length={value.length} />
+            )}
+          </>
+        )}
+      </button>
+      {!collapsed && (
+        <div className="pl-[10px]">
+          {value.map((child, index) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <div key={index}>
+              {child && (
+                <>
+                  <span
+                    className={classNames('text-console-object-blue font-bold')}
+                  >
+                    {index}
+                  </span>
+                  :{' '}
+                </>
+              )}
+              <Renderer
+                value={child}
+                nested
+                path={[...path, key]}
+                showDiffs={showDiffs}
+              />
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
+}
+
+function SimplePreview({length}: {length: number}) {
+  return <span className="text-white">Array({length})</span>;
+}
+
+function DescriptivePreview({
+  value,
+  showDiffs,
+}: {
+  value: any[];
+  showDiffs?: boolean;
+}) {
+  return (
+    <>
+      <span className="text-console-object-gray">({value.length})</span>
+      <span className="text-white">
+        {' '}
+        [
+        {value.map((child, index) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <React.Fragment key={index}>
+            <Renderer value={child} preview showDiffs={showDiffs} />
+            {index !== value.length - 1 && <>{', '}</>}
+          </React.Fragment>
+        ))}
+        ]
+      </span>
+    </>
   );
 }
