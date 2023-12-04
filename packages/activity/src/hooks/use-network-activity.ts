fa969084f7acb3d1730fd9922883bc1bcdf78d5f
Alexandre Clark <alex.clark.015@gmail.com>
2023-12-03 19:07:47 -0500

    Fix duplicate network request ids

--- packages/activity/src/hooks/use-network-activity.ts
+++ packages/activity/src/hooks/use-network-activity.ts
@@ -9,58 +9,66 @@ export function useNetworkActivity() {
 
   const networkActivity: ActivityStoreDescriptor<Snapshot<NetworkRequest>> =
     useMemo(() => {
-      const last = requests[requests.length - 1];
+      const snapshots = requests.map(createSnapshot);
 
       return {
         displayName: 'Network',
         type: 'network',
-        icon: (row) => {
-          if (row.nodes._resourceType === 'image') {
-            return row.nodes.request.url;
-          }
-
-          if (row.nodes._resourceType === 'script') {
-            return {
-              source: 'file-script',
-              variant: 'yellow',
-            };
-          }
-
-          return {
-            source: 'file-script',
-            variant: 'default',
-          };
-        },
-        rowName: (row) => {
-          if (!row?.nodes.request.url) {
-            return row.nodes._resourceType;
-          }
-          const urlParts = row.nodes.request.url.split('/');
-          const lastUrlPath = urlParts
-            .reverse()
-            .find((part) => part.length > 0);
-
-          if (!lastUrlPath || lastUrlPath.length === 0) {
-            return row.nodes._resourceType;
-          }
-
-          return lastUrlPath;
-        },
+        icon: getNetworkIcon,
+        rowName: getNetworkRowName,
         monitor: {
-          snapshot: {
-            id: last?.time.toString() + last?.request.url,
-            createdAt: last?.startedDateTime,
-            nodes: last,
-          },
-          snapshots: requests.map((request) => ({
-            id: request.time.toString() + request.request.url,
-            createdAt: request.startedDateTime,
-            nodes: request,
-          })),
-          clearSnapshots: () => clear(),
+          snapshot: snapshots[snapshots.length - 1],
+          snapshots,
+          clearSnapshots: clear,
         },
       };
     }, [clear, requests]);
 
   return networkActivity;
 }
+
+function getNetworkRowName(row: Snapshot<NetworkRequest>) {
+  if (!row?.nodes.request.url) {
+    return row.nodes._resourceType;
+  }
+  const urlParts = row.nodes.request.url.split('/');
+  const lastUrlPath = urlParts.reverse().find((part) => part.length > 0);
+
+  if (!lastUrlPath || lastUrlPath.length === 0) {
+    return row.nodes._resourceType;
+  }
+
+  return lastUrlPath.split('?')[0];
+}
+
+function createSnapshot(request: NetworkRequest): Snapshot<NetworkRequest> {
+  return {
+    id:
+      request.startedDateTime +
+      request.request.url +
+      request?.time +
+      request.request.method,
+    createdAt: request.startedDateTime,
+    nodes: request,
+  };
+}
+
+const getNetworkIcon: ActivityStoreDescriptor<
+  Snapshot<NetworkRequest>
+>['icon'] = (row) => {
+  if (row.nodes._resourceType === 'image') {
+    return row.nodes.request.url;
+  }
+
+  if (row.nodes._resourceType === 'script') {
+    return {
+      source: 'file-script',
+      variant: 'yellow',
+    };
+  }
+
+  return {
+    source: 'file-script',
+    variant: 'default',
+  };
+};
