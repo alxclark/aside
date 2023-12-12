a87396511a8eea6f2e8480e23dc307c2de9ce15d
Alexandre Clark <alex.clark.015@gmail.com>
2023-12-12 16:41:54 -0500

    Add dropdown doc

--- packages/chrome-ui/src/components/Dropdown/Dropdown.tsx
+++ packages/chrome-ui/src/components/Dropdown/Dropdown.tsx
@@ -23,20 +23,20 @@ const DropdownMenuContent = React.forwardRef<
   React.ElementRef<typeof DropdownMenuPrimitive.Content>,
   DropdownMenuContentProps
 >(({className, sideOffset = 4, ...props}, ref) => (
-  <DropdownMenuPrimitive.Portal>
-    <DropdownMenuPrimitive.Content
-      ref={ref}
-      side="bottom"
-      align="start"
-      sideOffset={3}
-      className={cn(
-        'z-50 min-w-[10rem] overflow-hidden bg-background border border-border shadow-lg py-1',
-        'data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
-        className,
-      )}
-      {...props}
-    />
-  </DropdownMenuPrimitive.Portal>
+  // <DropdownMenuPrimitive.Portal>
+  <DropdownMenuPrimitive.Content
+    ref={ref}
+    side="bottom"
+    align="start"
+    sideOffset={3}
+    className={cn(
+      'z-50 min-w-[10rem] overflow-hidden bg-background border border-border shadow-lg py-1',
+      'data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+      className,
+    )}
+    {...props}
+  />
+  // </DropdownMenuPrimitive.Portal>
 ));
 DropdownMenuContent.displayName = DropdownMenuPrimitive.Content.displayName;
 
