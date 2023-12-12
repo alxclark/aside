16588cd54d36d2c9ddb312c138e75a04a6f0037f
Alexandre Clark <alex.clark.015@gmail.com>
2023-12-11 21:44:47 -0500

    Simplify live sandbox to not require rendering Aside and Devtools

--- docs/src/components/AsideLive/AsideLive.tsx
+++ docs/src/components/AsideLive/AsideLive.tsx
@@ -4,11 +4,12 @@ import {
   Tabs,
   TabsList,
   TabsTrigger,
+  View,
 } from '@aside/chrome-ui';
 import {themes} from 'prism-react-renderer';
 import React, {type PropsWithChildren, useRef, useEffect} from 'react';
 import {LiveEditor, LiveError, LivePreview, LiveProvider} from 'react-live';
-import {createRoot} from 'react-dom/client';
+import {Root, createRoot} from 'react-dom/client';
 import sheet from '@aside/chrome-ui/css' assert {type: 'css'};
 
 import styles from './styles.module.css';
@@ -17,15 +18,18 @@ export interface Props {
   code: string;
   scope?: {[key: string]: unknown} | undefined;
   height?: number;
+  showFrame?: boolean;
+  centered?: boolean;
 }
 
 export function Shadow(props: PropsWithChildren) {
   const divRef = useRef<HTMLDivElement>();
   const shadowRef = useRef<ShadowRoot>();
   const elementForShadowRef = useRef<HTMLDivElement>();
+  const rootRef = useRef<Root>();
 
   useEffect(() => {
-    if (!shadowRef.current) {
+    if (!rootRef.current) {
       const elementForShadow = document.createElement('div');
       const shadow = divRef.current.attachShadow({mode: 'open'});
 
@@ -34,20 +38,18 @@ export function Shadow(props: PropsWithChildren) {
 
       shadowRef.current = shadow;
       elementForShadowRef.current = elementForShadow;
+      const root = createRoot(elementForShadowRef.current);
+
+      rootRef.current = root;
     }
 
-    const root = createRoot(elementForShadowRef.current);
-    root.render(props.children);
-
-    return () => {
-      root.unmount();
-    };
+    rootRef.current.render(props.children);
   }, [props]);
 
   return <div ref={divRef} />;
 }
 
-export function AsideLive({code, scope, height}: Props) {
+export function AsideLive({code, scope, height, ...rest}: Props) {
   return (
     <LiveProvider
       enableTypeScript
@@ -56,41 +58,67 @@ export function AsideLive({code, scope, height}: Props) {
       scope={{
         React,
         ...React,
-        Aside: ({children}: PropsWithChildren) => (
-          <>
-            <div className={styles.container}>{children}</div>
-          </>
-        ),
+        Aside,
         Devtools: ({children}: PropsWithChildren) => (
-          <Shadow>
-            <div
-              style={{
-                minHeight: 100,
-                fontSize: 12,
-                fontFamily: 'Helvetica Neue',
-              }}
-              className="aside bg-background"
-            >
-              <Tabs defaultValue="aside">
-                <PaneToolbar>
-                  <TabsList>
-                    <TabsTrigger value="aside">Aside</TabsTrigger>
-                  </TabsList>
-                </PaneToolbar>
-              </Tabs>
-              {children}
-            </div>
-          </Shadow>
+          <Devtools centered>{children}</Devtools>
         ),
         ...AllComponents,
         ...scope,
       }}
     >
       <LiveError />
-      <LivePreview style={{height}} />
+      <LivePreview
+        Component={({children}) => (
+          <Aside>
+            <Devtools {...rest}>{children}</Devtools>
+          </Aside>
+        )}
+        style={{height}}
+      />
       <code className={styles.code}>
         <LiveEditor tabMode="focus" className={styles.editor} />
       </code>
     </LiveProvider>
+  );
+}
+
+function Aside({children}: PropsWithChildren<{}>) {
+  return <div className={styles.container}>{children}</div>;
+}
+
+function Devtools({
+  children,
+  centered,
+  showFrame,
+}: PropsWithChildren<Pick<Props, 'centered' | 'showFrame'>>) {
+  return (
+    <Shadow>
+      <div
+        style={{
+          minHeight: 100,
+          fontSize: 12,
+          fontFamily: 'Helvetica Neue',
+          zoom: 1.25,
+          display: centered ? 'flex' : 'block',
+        }}
+        className="aside bg-background"
+      >
+        {showFrame && (
+          <Tabs defaultValue="aside">
+            <PaneToolbar>
+              <TabsList>
+                <TabsTrigger value="aside">Aside</TabsTrigger>
+              </TabsList>
+            </PaneToolbar>
+          </Tabs>
+        )}
+        {centered && (
+          <View className="flex justify-center items-center grow">
+            {children}
+          </View>
+        )}
+        {!centered && children}
+      </div>
+    </Shadow>
   );
 }
