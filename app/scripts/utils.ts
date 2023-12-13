779df9c6de246f3ac0a3ee15294f13d36e9cbcb9
Alexandre Clark <alex.clark.015@gmail.com>
2023-12-13 15:10:32 -0500

    Replace esno by tsx

--- app/scripts/utils.ts
+++ app/scripts/utils.ts
@@ -1,7 +1,11 @@
 /* eslint-disable id-length */
-import {resolve} from 'path';
+import {resolve, dirname} from 'path';
+import {fileURLToPath} from 'url';
 
 import {bgCyan, black} from 'kolorist';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const port = parseInt(process.env.PORT || '', 10) || 8888;
 export const r = (...args: string[]) => resolve(__dirname, '..', ...args);
