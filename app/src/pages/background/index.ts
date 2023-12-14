c721834e813b1d519bebda7614e617554f72679f
Alexandre Clark <alex.clark.015@gmail.com>
2023-12-14 14:47:46 -0500

    Update background page to still subscribe to messages from the runtime

--- app/src/pages/background/index.ts
+++ app/src/pages/background/index.ts
@@ -1,22 +1,24 @@
 import {
-  // WEBPAGE_INITIATED_CONNECTION,
+  WEBPAGE_INITIATED_CONNECTION,
   setupContentScriptHMR,
 } from '../../foundation/ContentScript';
-// import {isForbiddenUrl} from '../../env';
+import {isForbiddenUrl} from '../../env';
 
 if (import.meta.env.DEV) {
   setupContentScriptHMR();
 }
 
 async function background() {
-  // browser.webNavigation.onCommitted.addListener(({frameId, url}) => {
-  //   // Filter out non main window events.
-  //   if (frameId !== 0) return;
+  browser.webNavigation.onCommitted.addListener(({frameId, url, tabId}) => {
+    // Filter out non main window events.
+    if (frameId !== 0) return;
 
-  //   if (isForbiddenUrl(url)) return;
+    if (isForbiddenUrl(url)) return;
 
-  //   browser.action.setBadgeText({text: 'OFF', tabId});
-  // });
+    console.log({tabId});
+
+    // browser.action.setBadgeText({text: 'OFF', tabId});
+  });
 
   // When the content-script attempts to connect to the devtools panel,
   // it expects at least one listener to be active. Since in most cases,
@@ -24,19 +26,20 @@ async function background() {
   // to provide a dummy listener to prevent the connection from failing.
   browser.runtime.onConnect.addListener(() => {});
 
-  // browser.runtime.onMessage.addListener(async (message, sender) => {
-  //   if (message.type === WEBPAGE_INITIATED_CONNECTION) {
-  //     browser.action.setIcon({
-  //       tabId: sender?.tab?.id,
-  //       path: {
-  //         16: '/assets/logo-16-disabled.png',
-  //         48: '/assets/logo-16-disabled.png',
-  //         128: '/assets/logo-16-disabled.png',
-  //       },
-  //     });
-  //     return true;
-  //   }
-  // });
+  browser.runtime.onMessage.addListener(async (message, sender) => {
+    if (message.type === WEBPAGE_INITIATED_CONNECTION) {
+      console.log('connected', sender);
+      // browser.action.setIcon({
+      //   tabId: sender?.tab?.id,
+      //   path: {
+      //     16: '/assets/logo-16-disabled.png',
+      //     48: '/assets/logo-16-disabled.png',
+      //     128: '/assets/logo-16-disabled.png',
+      //   },
+      // });
+      return true;
+    }
+  });
 }
 
 background();
