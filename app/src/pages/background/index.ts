cf900ee345c0411fe7d4cf7182ee37b9940b66e3
Alexandre Clark <alex.clark.015@gmail.com>
2023-12-13 19:55:33 -0500

    Fix CI

--- app/src/pages/background/index.ts
+++ app/src/pages/background/index.ts
@@ -9,14 +9,14 @@ if (import.meta.env.DEV) {
 }
 
 async function background() {
-  browser.webNavigation.onCommitted.addListener(({tabId, frameId, url}) => {
-    // Filter out non main window events.
-    if (frameId !== 0) return;
+  // browser.webNavigation.onCommitted.addListener(({frameId, url}) => {
+  //   // Filter out non main window events.
+  //   if (frameId !== 0) return;
 
-    if (isForbiddenUrl(url)) return;
+  //   if (isForbiddenUrl(url)) return;
 
-    // browser.action.setBadgeText({text: 'OFF', tabId});
-  });
+  //   browser.action.setBadgeText({text: 'OFF', tabId});
+  // });
 
   // When the content-script attempts to connect to the devtools panel,
   // it expects at least one listener to be active. Since in most cases,
@@ -24,20 +24,19 @@ async function background() {
   // to provide a dummy listener to prevent the connection from failing.
   browser.runtime.onConnect.addListener(() => {});
 
-  browser.runtime.onMessage.addListener(async (message, sender) => {
-    if (message.type === WEBPAGE_INITIATED_CONNECTION) {
-      // This doesn't seem to work
-      // browser.action.setIcon({
-      //   tabId: sender?.tab?.id,
-      //   path: {
-      //     16: '/assets/logo-16-disabled.png',
-      //     48: '/assets/logo-16-disabled.png',
-      //     128: '/assets/logo-16-disabled.png',
-      //   },
-      // });
-      return true;
-    }
-  });
+  // browser.runtime.onMessage.addListener(async (message, sender) => {
+  //   if (message.type === WEBPAGE_INITIATED_CONNECTION) {
+  //     browser.action.setIcon({
+  //       tabId: sender?.tab?.id,
+  //       path: {
+  //         16: '/assets/logo-16-disabled.png',
+  //         48: '/assets/logo-16-disabled.png',
+  //         128: '/assets/logo-16-disabled.png',
+  //       },
+  //     });
+  //     return true;
+  //   }
+  // });
 }
 
 background();
