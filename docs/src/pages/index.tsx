12c936fd8a136dbf54bcc2c28f286fac8104bcd4
Alexandre Clark <alex.clark.015@gmail.com>
2023-12-17 23:02:33 -0500

    Add basic home page for docs

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -2,7 +2,6 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
@@ -24,14 +23,46 @@ function HomepageHeader() {
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title="Aside developer tools"
-      description="Description will go into a meta tag in <head />"
-    >
-      <HomepageHeader />
-      {/* <main>
-        <HomepageFeatures />
-      </main> */}
-    </Layout>
+    <div className={styles.page}>
+      <Layout
+        title="Aside developer tools"
+        description="Description will go into a meta tag in <head />"
+      >
+        <div className={styles.container}>
+          <div className={styles.intro}>
+            <div>
+              <img
+                className={styles.arrow}
+                src="/img/curly-dashed.svg"
+                alt=""
+              />
+            </div>
+            <Heading as="h1" className="hero__title">
+              The extensible <br /> developer tools
+            </Heading>
+            <p className={styles.text}>
+              Aside is a Chrome extension that can render{' '}
+              <strong>UI natively in the developer tools panel</strong>,
+              controlled by your own React website. <br />
+              <br />
+              It exposes a variety of APIs to empower your developer experience,
+              without the friction of traditional Chrome extensions.
+            </p>
+            <div className={styles.buttons}>
+              <Link className={styles.button} to="/docs/getting-started">
+                Get started
+              </Link>
+              <Link
+                className={clsx(styles.button, styles['button--primary'])}
+                to="https://aside.dev/download"
+              >
+                Download
+              </Link>
+            </div>
+          </div>
+          <img className={styles.code} src="/img/code.png" alt="" />
+        </div>
+      </Layout>
+    </div>
   );
 }
